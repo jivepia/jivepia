8730f26c3fd52b4f98569e27434bd4be21d263e8
hans <hans@Hans-MacBook-Pro.local>
2015-01-05 18:18:15 +0900

    Initial Commit

---
+++ testGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testGitHub
+//
+//  Created by hans on 2015. 1. 5..
+//  Copyright (c) 2015년 hans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
